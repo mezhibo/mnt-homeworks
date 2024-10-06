ac498e72fa3583ef7a436dd11215f750115170cc
Mezhibo Evgeniy <mezhibo@example.com>
2024-10-06 19:33:53 +0300

    1

---
+++ 09-ci-06-gitlab/terraform/vars.resources.tf
@@ -0,0 +1,52 @@
+### vms_resources
+variable "vms_resources" {
+  type = map(map(number))
+  default = {
+    nexus = {
+      cores    = 4
+      memory   = 4
+      fraction = 50
+    }
+    vector = {
+      cores    = 2
+      memory   = 2
+      fraction = 5
+    }
+    lighthouse = {
+      cores    = 2
+      memory   = 2
+      fraction = 5
+    }
+  }
+}
+
+variable "vm_os_centos" {
+  type        = string
+  default     = "centos-7"
+  description = "ubuntu version"
+}
+
+variable "vm_cpu_id_v1" {
+  type        = string
+  default     = "standard-v1"
+  description = "cpu id v1"
+}
+
+### vm_name/hostname
+variable "vm_01" {
+  type        = string
+  default     = "gitlab"
+  description = "vm name"
+}
+
+variable "vm_02" {
+  type        = string
+  default     = "runner"
+  description = "vm name"
+}
+
+variable "vm_03" {
+  type        = string
+  default     = "lighthouse"
+  description = "vm name"
+}
---
+++ 09-ci-06-gitlab/terraform/vm.tf
@@ -0,0 +1,111 @@
+### ===clickhouse===
+# data "yandex_compute_image" "centos-clickhouse" {
+#   family = var.vm_os_centos
+# }
+resource "yandex_compute_instance" "gitlab" {
+  name        = var.vm_01
+  hostname    = var.vm_01
+  platform_id = var.vm_cpu_id_v1
+  resources {
+    cores         = 4
+    memory        = 4
+    core_fraction = 100
+  }
+  metadata = local.metadata
+  boot_disk {
+    initialize_params {
+      image_id = "fd874d4jo8jbroqs6d7i"
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id  = yandex_vpc_subnet.subnet_db.id
+    nat        = true
+    ip_address = "10.10.10.11"
+  }
+}
+
+resource "yandex_compute_instance" "runner" {
+  name        = var.vm_02
+  hostname    = var.vm_02
+  platform_id = var.vm_cpu_id_v1
+  resources {
+    cores         = 4
+    memory        = 4
+    core_fraction = 100
+  }
+  metadata = local.metadata
+  boot_disk {
+    initialize_params {
+      image_id = "fd874d4jo8jbroqs6d7i"
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id  = yandex_vpc_subnet.subnet_db.id
+    nat        = true
+    ip_address = "10.10.10.12"
+  }
+}
+
+# ### ===vector===
+# data "yandex_compute_image" "centos-vector" {
+#   family = var.vm_os_centos
+# }
+# resource "yandex_compute_instance" "vector" {
+#   name        = var.vm_02
+#   hostname    = var.vm_02
+#   platform_id = var.vm_cpu_id_v1
+#   resources {
+#     cores         = var.vms_resources.vector.cores
+#     memory        = var.vms_resources.vector.memory
+#     core_fraction = var.vms_resources.vector.fraction
+#   }
+#   metadata = local.metadata
+#   boot_disk {
+#     initialize_params {
+#       image_id = data.yandex_compute_image.centos-vector.image_id
+#     }
+#   }
+#   scheduling_policy {
+#     preemptible = true
+#   }
+#   network_interface {
+#     subnet_id  = yandex_vpc_subnet.subnet_db.id
+#     nat        = true
+#     ip_address = "10.10.10.12"
+#   }
+# }
+
+# ### ===lighthouse===
+# data "yandex_compute_image" "centos-lighthouse" {
+#   family = var.vm_os_centos
+# }
+# resource "yandex_compute_instance" "lighthouse" {
+#   name        = var.vm_03
+#   hostname    = var.vm_03
+#   platform_id = var.vm_cpu_id_v1
+#   resources {
+#     cores         = var.vms_resources.lighthouse.cores
+#     memory        = var.vms_resources.lighthouse.memory
+#     core_fraction = var.vms_resources.lighthouse.fraction
+#   }
+#   metadata = local.metadata
+#   boot_disk {
+#     initialize_params {
+#       image_id = data.yandex_compute_image.centos-lighthouse.image_id
+#     }
+#   }
+#   scheduling_policy {
+#     preemptible = true
+#   }
+#   network_interface {
+#     subnet_id  = yandex_vpc_subnet.subnet_db.id
+#     nat        = true
+#     ip_address = "10.10.10.13"
+#   }
+# }
